82c098adb03d941e32822066949e6771a063d790
zzekrozz <pogrebnyakivan123@gmail.com>
2026-05-29 07:19:11 +0200

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import Landing from '@/components/landing/Landing';
-
-export default function HomePage() {
-  return <Landing demoHref="/demo" buyHref="/app/dashboard" premiumHref="/app/acompanamiento" />;
-}
