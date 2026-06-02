e9c1fc9d765fcca42b006a36ed63a46f4b8abc7f
zzekrozz <pogrebnyakivan123@gmail.com>
2026-06-02 16:17:10 +0200

    Delete src directory

--- src/app/app/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { AppShell } from '@/components/shell/AppShell';
-
-export default function AppLayout({ children }: { children: React.ReactNode }) {
-  return <AppShell>{children}</AppShell>;
-}
